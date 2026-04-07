419ec895278f92d25e4016ec79e31ee945b15413
Rachel <raquelrenterohernandez@gmail.com>
2026-04-07 15:57:31 +0200

    login.spec.ts

---
+++ tests/login.spec.ts
@@ -0,0 +1,20 @@
+import { test } from '@playwright/test'
+import { CommonPageMethods } from '../pages/common-page/common-page.methods'
+import { LoginPageMethods } from '../pages/login-page/login-page.methods'
+import { loginPageData } from '../pages/login-page/login-page.data'
+
+const userCredentials = loginPageData.credentials;
+
+test.describe('Login', () => {
+    
+    test('Login with valid credentials', async({page})=>{
+        const commonPageMethods = new CommonPageMethods(page)
+        const loginPageMethods = new LoginPageMethods(page)
+
+        await commonPageMethods.navigateToTheApplication()
+        await loginPageMethods.insertUsername(userCredentials.usernames.standardUser)
+        await loginPageMethods.insertPassword(userCredentials.password)
+        await loginPageMethods.clickLoginButton()
+    })
+})
+   
